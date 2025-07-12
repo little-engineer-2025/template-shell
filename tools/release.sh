13ddae4b59f4595defddf78f7505703fca56c30b
Alejandro Visiedo <alejandro.visiedo@gmail.com>
2025-07-13 01:22:00 +0200

    fix: tools/release.sh
    - Update rules to check if state is ready for release.
    - Fix some issues when generating the tag annotations.
    
    Signed-off-by: Alejandro Visiedo <alejandro.visiedo@gmail.com>

--- tools/release.sh
+++ tools/release.sh
@@ -5,30 +5,17 @@
 # @brief Script for tagging with a changelog the repository.
 ##
 
+# @describe Check if the version is ready for release
 release_is_version() {
     local version="$1"
-    [[ "${version}" =~ ^v([:digit:]+).([:digit:]+).([:digit:]+)$ ]]
-}
-
-release_is_on_sync() {
-    local version="$1"
-    ! release_is_version "${version}" && [[ "${version}" =~ ^v[:digit:]+.[:digit:]+.[:digit:]+(-[:digit:]+)?(-dirty)?$ ]]
-}
-
-release_is_dirty() {
-    local version="$1"
-    ! release_is_version "${version}" && [[ "${version}" =~ ^v[:digit:]+.[:digit:]+.[:digit:]+(-[:digit:]+)?-dirty$ ]]
+    [[ "${version}" =~ ^v([[:digit:]]+)\.([[:digit:]]+)\.([[:digit:]]+)$ ]]
 }
 
 # @describe Check version string
 release_version_check() {
     local version="$1"
-    ! release_is_on_sync "${version}" || {
-        print "error: there are changes pending to merge\n" >&2
-        exit 1
-    }
-    ! release_is_dirty "${version}" || {
-        print "error: there are changes pending to commit\n" >&2
+    release_is_version "${version}" || {
+        printf "error: the %s version is not ready for release\n" "${version}"
         exit 1
     }
 }
@@ -43,7 +30,7 @@ release_generate_changelog() {
 
     # Get the last tagged commit
     local LAST_TAG_COMMIT
-    LAST_TAG_COMMIT="$(git describe --tags --abrev=0 2>/dev/null)"
+    LAST_TAG_COMMIT="tags/$(git describe --tags HEAD^ 2>/dev/null)"
 
     local BEGIN_COMMIT
 
@@ -53,8 +40,7 @@ release_generate_changelog() {
     else
         # From the first repo commit
         BEGIN_COMMIT="${LAST_TAG_COMMIT}"
-        version="v1.0.0-$(git rev-parse --short HEAD)"
-        printf "Changes:\n"
+        version="v1.0.0"
     fi
 
     # Get the commit range selecting by conventional commit types
@@ -75,26 +61,21 @@ release_generate_changelog() {
     version_patch="${version_patch%%-*}"
 
     # Check for broken changes
-    if [ "$(git log "${BEGIN_COMMIT}..HEAD" --grep="^!")" != "" ]; then
+    if [ "$(git log "${BEGIN_COMMIT}..HEAD" --grep="^!" 2>&1)" != "" ]; then
         version_major=$((version_major + 1))
         version_minor="0"
         version_patch="0"
-    elif [ "$(git log "${BEGIN_COMMIT}..HEAD" --grep="^feat:")" != "" ]; then
+    elif [ "$(git log "${BEGIN_COMMIT}..HEAD" --grep="^feat:" 2>&1)" != "" ]; then
         version_minor=$((version_minor + 1))
         version_patch="0"
-    elif [ "$(git log "${BEGIN_COMMIT}..HEAD" --grep="^fix:")" != "" ]; then
+    elif [ "$(git log "${BEGIN_COMMIT}..HEAD" --grep="^fix:" 2>&1)" != "" ]; then
         version_patch=$((version_patch + 1))
     fi
+    version="v${version_major}.${version_minor}.${version_patch}"
 
     # Print the log output
     printf "Changes on %s:\n" "${version}"
     printf "%s\n" "${COMMITS}"
-}
-
-# @describe tag current commit
-release_tag_commit() {
-    local version="$1"
-    git tag -a "${version}" -m "TAG_MESSAGE"
 }
 
 # @describe Release entrypoint
