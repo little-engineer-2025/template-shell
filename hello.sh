2008dacf4eaa3a617c85fb9aeedb865791936a37
Alejandro Visiedo <alejandro.visiedo@gmail.com>
2025-07-28 17:50:54 +0200

    refactor: encapsulate main check
    - Refactor and expand check for scenarios like:
      - ./hello.sh not in the PATH but directly referenced.
      - hello.sh when the script is in the PATH
    
    Signed-off-by: Alejandro Visiedo <alejandro.visiedo@gmail.com>

--- hello.sh
+++ hello.sh
@@ -3,7 +3,7 @@
 # SPDX-License-Identifier: MIT
 # Copyright (c) 9999  First and Last name <email@example.com>
 
-[ "${BASH_SOURCE[0]}" == "$0" ] && BASEDIR="$(dirname "$(realpath "$0")")"
+[ "$(realpath "${BASH_SOURCE[0]}")"  == "$(realpath "$0")" ] && BASEDIR="$(dirname "$(realpath "$0")")"
 
 # shellcheck disable=SC1091
 source "${BASEDIR}/lib/log.lib.sh"
@@ -12,6 +12,7 @@ main() {
     echo "Hello World!"
 }
 
-if [ "${BASH_SOURCE[0]}" == "$0" ]; then
+if [ "$(realpath "${BASH_SOURCE[0]}")"  == "$(realpath "$0")" ]; then
     main "$@"
+    exit $?
 fi
